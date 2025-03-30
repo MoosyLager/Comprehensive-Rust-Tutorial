12712d3cd3c7d4232f62e024d312780d3ecfe0d3
Shenal Herath <smh231@uclive.ac.nz>
2025-03-30 22:56:56 +1300

    exercise 6.8 complete

--- collatzSequence/src/main.rs
+++ collatzSequence/src/main.rs
@@ -1,6 +1,21 @@
 /// Determine the length of the collatz sequence beginning at `n`.
 fn collatz_length(mut n: i32) -> u32 {
-    todo!("Implement this")
+    let mut length = 1;
+    // while n != 1 {
+    //     if n % 2 == 0 {
+    //         n = n / 2;
+    //         length += 1;
+    //     } else {
+    //         n = 3 * n + 1;
+    //         length += 1;
+    //     };
+    // }
+    while n > 1 {
+        n = if n % 2 == 0 { n / 2 } else { 3 * n + 1 };
+        length += 1;
+    }
+    length
+    // dbg!(n);
 }
 
 fn main() {
