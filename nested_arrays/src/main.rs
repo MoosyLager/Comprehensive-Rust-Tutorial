140a3459cf4919aab56d5c28031e970c50c828b8
Shenal Herath <smh231@uclive.ac.nz>
2025-04-02 13:30:11 +1300

    updated collatz_sequence name and added new exercise 8.5

---
+++ nested_arrays/src/main.rs
@@ -0,0 +1,13 @@
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {}
+
+fn main() {
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    dbg!(matrix);
+    let transposed = transpose(matrix);
+    dbg!(transposed);
+}
