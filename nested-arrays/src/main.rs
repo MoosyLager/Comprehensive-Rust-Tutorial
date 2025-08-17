1b7bc9ca57da54d1dd07f04e86b676f60b08e6ef
Shenal Herath <smh231@uclive.ac.nz>
2025-08-17 19:21:14 +1200

    exercise: nested arrays completed

--- nested-arrays/src/main.rs
+++ nested-arrays/src/main.rs
@@ -1,3 +1,21 @@
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let mut transposed: [[i32; 3]; 3] = [[0; 3]; 3];
+    for i in 0..3 {
+        for j in 0..3 {
+            transposed[j][i] = matrix[i][j];
+        }
+    }
+    transposed
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matrix = [
+        [101, 102, 103], //
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    dbg!(matrix);
+    let transposed = transpose(matrix);
+    dbg!(transposed);
 }
