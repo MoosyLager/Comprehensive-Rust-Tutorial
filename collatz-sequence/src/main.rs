896adba730de001c5e42c412c0b12174cbbb9d49
Shenal Herath <smh231@uclive.ac.nz>
2025-08-17 17:48:18 +1200

    completed exercise: collatz sequence

--- collatz-sequence/src/main.rs
+++ collatz-sequence/src/main.rs
@@ -1,3 +1,18 @@
+/// Determine the length of the collatz sequence beginning at `n`.
+fn collatz_length(mut n: i32) -> u32 {
+    let mut length = 1;
+    while n != 1 {
+        if n % 2 == 0 {
+            n /= 2;
+            length += 1;
+        } else {
+            n = 3 * n + 1;
+            length += 1;
+        }
+    }
+    return length;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Length: {}", collatz_length(11)); // should be 15
 }
